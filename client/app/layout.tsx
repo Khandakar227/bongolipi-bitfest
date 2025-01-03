b9438253f3c03759ea4fb58533d5b6e9952ed314
Kashshaf-Labib <labibkashshaf@gmail.com>
2025-01-03 13:14:21 +0600

    Navbar reimported

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/common/Navbar";
 
 export const metadata: Metadata = {
   title: "BongoLipi",
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <ClerkProvider dynamic>
       <html lang="en">
-        <body className={`font-poppins antialiased`}>{children}</body>
+        <body className={`font-poppins antialiased`}>
+          <Navbar/>
+          {children}</body>
       </html>
     </ClerkProvider>
   );
