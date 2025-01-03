e687866ec066459282c83607395dd9ec1d7f9799
Khandakar227 <xenonknight54@gmail.com>
2025-01-03 16:10:25 +0600

    Added content page form

--- client/app/contents/page.tsx
+++ client/app/contents/page.tsx
@@ -1,11 +1,12 @@
 import { Plus } from "lucide-react"
+import Link from "next/link"
 
 function Contents() {
   return (
-    <div className="min-h-screen py-12 max-w-7xl mx-auto">
+    <div className="min-h-screen py-12 max-w-7xl mx-auto px-4">
       <div className="flex justify-between items-center gap-4">
         <h1 className="text-4xl font-bold">Manage your Contents</h1>
-        <button className="flex justify-center items-center gap-2 bg-primary p-2 pr-4 rounded text-white"><Plus/><span>Create</span></button>
+        <Link href={"/contents/create"} className="flex justify-center items-center gap-2 bg-primary p-2 pr-4 rounded text-white"><Plus/><span>Create</span></Link>
       </div>
     </div>
   )
